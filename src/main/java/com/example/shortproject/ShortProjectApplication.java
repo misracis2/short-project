eb1358efea5ed7f59a892352725a14fd2a17d206
seungyeol lee <misracis@gmail.com>
2023-04-04 19:25:16 +0900

    extra commint

---
+++ src/main/java/com/example/shortproject/ShortProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortProjectApplication.class, args);
+    }
+
+}
