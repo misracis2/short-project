e490a75f8ec3bf5fe491901d94cc3e3482d4aacc
seungyeol lee <misracis@gmail.com>
2023-04-05 20:05:41 +0900

    학생 entity 수정(name, memberRole 추가)

--- src/main/java/com/example/shortproject/entity/Student.java
+++ src/main/java/com/example/shortproject/entity/Student.java
@@ -1,20 +1,35 @@
 package com.example.shortproject.entity;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
 import java.util.List;
 
 @Entity
 @NoArgsConstructor
+@Getter
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long studentId;
 
+    private String name;
+
+    @Max(3)
     private int enrollmentNumber;
 
-    @OneToMany(mappedBy = "student")
+    @Enumerated(EnumType.STRING)
+    private MemberRole memberRole;
+
+    @OneToMany(mappedBy = "student", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<Enrollment> enrollmentList;
+
+    public Student(String name) {
+        this.name = name;
+        this.enrollmentNumber = 0;
+        this.memberRole = MemberRole.STUDENT;
+    }
 }
---
+++ src/main/java/com/example/shortproject/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.shortproject.repository;
+
+import com.example.shortproject.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
