bf386481cd3511e3997ba14f652477abc0b1a837
seungyeol lee <misracis@gmail.com>
2023-04-05 18:25:47 +0900

    교수 회원 정보 변경

---
+++ src/main/java/com/example/shortproject/conroller/ProfessorController.java
@@ -0,0 +1,34 @@
+package com.example.shortproject.conroller;
+
+import com.example.shortproject.dto.CourseDto;
+import com.example.shortproject.service.ProfessorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/professor")
+public class ProfessorController {
+
+    private final ProfessorService professorService;
+
+    @GetMapping("/course/{courseId}")
+    public CourseDto.ResponseDto getCourse(@PathVariable Long courseId) {
+        return professorService.getCourse(courseId);
+    }
+
+    @GetMapping("/{professorId}/course")
+    public List<CourseDto.ResponseDto> getCourseList(@PathVariable Long professorId) {
+        return professorService.getCourseList(professorId);
+    }
+
+    @GetMapping("/course/{courseId}/student")
+    public void getStudent(@PathVariable long courseId){
+
+    }
+}
---
+++ src/main/java/com/example/shortproject/entity/Professor.java
@@ -0,0 +1,35 @@
+package com.example.shortproject.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long professorId;
+
+    private String name;
+
+    @Max(2)
+    private int courseNumber;
+
+    @Enumerated(EnumType.STRING)
+    private MemberRole memberRole;
+
+    @OneToMany(mappedBy = "professor", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Course> CourseList;
+
+    public Professor(String name) {
+        this.name = name;
+        this.courseNumber = 0;
+        this.memberRole = MemberRole.PROFESSOR;
+    }
+}
