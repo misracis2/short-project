ce8b817d5245f1ab4ba56b6b9f501342a1285bd9
seungyeol lee <misracis@gmail.com>
2023-04-05 18:50:33 +0900

    교수 강의 개수 제한 확인 로직 추가

--- src/main/java/com/example/shortproject/service/AdminService.java
+++ src/main/java/com/example/shortproject/service/AdminService.java
@@ -7,6 +7,7 @@ import com.example.shortproject.repository.CourseRepository;
 import com.example.shortproject.repository.ProfessorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,13 +17,20 @@ public class AdminService {
 
     private final CourseRepository courseRepository;
 
+    @Transactional
     public void addCourses(AdminDto.AddCoursesDto addCoursesDto) {
         //담당 교수 확인
         Professor professor = professorRepository.findByName(addCoursesDto.getProfessorName()).orElseThrow(
                 ()-> new IllegalArgumentException("존재하지 않는 교수명입니다.")
         );
-        //강의 추가
-        courseRepository.save(new Course(addCoursesDto.getTitle(), addCoursesDto.getStudentNumber(), professor));
+        //강의 개수 확인
+        if(professor.getCourseNumber() >= 2){
+            throw new IllegalArgumentException("최대 강의수를 초과합니다.");
+        }else {
+            //강의 추가
+            courseRepository.save(new Course(addCoursesDto.getTitle(), addCoursesDto.getStudentNumber(), professor));
+            professor.increaseCourseNumber();
+        }
 
     }
 }
