dfe041dafc85981a9b54aace81ab0e4840704e5d
seungyeol lee <misracis@gmail.com>
2023-04-06 03:32:07 +0900

    비관적 락 설정 추가

--- src/main/java/com/example/shortproject/repository/CourseRepository.java
+++ src/main/java/com/example/shortproject/repository/CourseRepository.java
@@ -2,7 +2,15 @@ package com.example.shortproject.repository;
 
 import com.example.shortproject.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import javax.persistence.LockModeType;
+import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
+    //동시 수강신청 처리를 위한 비관적 락 설정
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Override
+    Optional<Course> findById(Long aLong);
 }
